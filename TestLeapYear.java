06253d098e1f8d503915e3bbf83de6f45ed0fd00
M-G-Olsen <89856912+M-G-Olsen@users.noreply.github.com>
2021-10-31 09:29:54 +0100

    Add files via upload

---
+++ TestLeapYear.java
@@ -0,0 +1,39 @@
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class TestLeapYear {
+    @Test
+    @MethodSource("LeapYear")
+    public void Test_false_if_divisible_by_100_but_not_400() {
+        assertFalse(LeapYear.isLeapYear(1700));
+        assertFalse(LeapYear.isLeapYear(2500));
+    }
+
+    @Test
+    @MethodSource("LeapYear")
+    public void Test_divisible_by_400() {
+        assertTrue(LeapYear.isLeapYear(2000));
+        assertTrue(LeapYear.isLeapYear(4000));
+    }
+
+    @Test
+    @MethodSource("LeapYear")
+    public void Test_year_is_leapyear() {
+        assertTrue(LeapYear.isLeapYear(2016));
+        assertTrue(LeapYear.isLeapYear(1996));
+        assertTrue(LeapYear.isLeapYear(3004));
+        assertTrue(LeapYear.isLeapYear(1784));
+    }
+
+    @Test
+    @MethodSource("LeapYear")
+    public void Test_year_is_not_leapyear() {
+        assertFalse(LeapYear.isLeapYear(1775));
+        assertFalse(LeapYear.isLeapYear(1999));
+        assertFalse(LeapYear.isLeapYear(2207));
+    }
+
+}
